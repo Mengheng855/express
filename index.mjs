ee63b44f688f2c402ac30c17061f02f5342e5bb8
Mengheng855 <mengheng.mh855@gmail.com>
2025-11-24 14:09:50 +0700

    crud_mvc

--- index.mjs
+++ index.mjs
@@ -1,7 +1,14 @@
 import express from 'express'
+import  cors from 'cors'
 import { router } from './routes/user.mjs'
 const app=express()
 app.use(express.json())
+app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET','POST','PATCH','DELETE','PUT'],
+    allowedHeaders: ['Content-Type']
+}))
 app.use('/',router)
 app.listen(3000,()=>{
     console.log("Server is running on http://localhost:3000");
